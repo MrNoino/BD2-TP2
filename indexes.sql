a3534eca67930c718f9c8027edbdd18cff005028
MrNoino <nuno.miguelsl25@gmail.com>
2023-06-23 14:32:33 +0100

    Updated database indexes

--- indexes.sql
+++ indexes.sql
@@ -1,3 +1,9 @@
+DROP INDEX IF EXISTS system_index;
+
+DROP INDEX IF EXISTS alert_index;
+
+DROP INDEX IF EXISTS user_index;
+
 CREATE INDEX system_index on public."System"(id, owner_id);
 
 CREATE INDEX alert_index on public."Alert"(id, sensor_id);
